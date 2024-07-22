918e1cb0693c70d2da7a51431a1e675fb34b0779
Alex Naupay <alexnaupay@gmail.com>
2024-07-21 21:16:48 -0500

    Add fonts and apply to app theme

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        fontFamily: "Poppins"
       ),
       //home: const MyHomePage(title: 'Flutter Demo Home Page'),
         home: Scaffold(
